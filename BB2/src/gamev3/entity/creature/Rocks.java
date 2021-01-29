528fca1dd28074cdd8565683026d87fba647de5f
5ss5sin <64581381+5ss5sin@users.noreply.github.com>
2021-01-29 09:18:22 +0800

    random rock size per rock object

--- BB2/src/gamev3/entity/creature/Rocks.java
+++ BB2/src/gamev3/entity/creature/Rocks.java
@@ -12,6 +12,8 @@ public class Rocks extends Entity{
 	private Handler handler;
 	public final static int ROCK_WIDTH = 10;
 	public final static int ROCK_HEIGHT = 10;
+	//random rock size woooo
+	private int rock_size = (int)(Math.random() * (50 - 10 + 1)+ 10);
 	
 	public Rocks(Handler handler, float x, float y) {
 		super(handler, x, y, ROCK_WIDTH, ROCK_HEIGHT);
@@ -28,7 +30,7 @@ public class Rocks extends Entity{
 	@Override
 	public void render(Graphics g) {
 		// TODO Auto-generated method stub
-		g.drawImage(Assets.rocks, (int)x,(int)y,10,10,null);
+		g.drawImage(Assets.rocks, (int)x,(int)y,rock_size,rock_size,null);
 	}
 
 }
