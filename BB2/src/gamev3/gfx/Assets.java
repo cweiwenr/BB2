9fd89fa974c220331ac3f23186a04005a55abbae
Nigouz <75168577+Nigouz@users.noreply.github.com>
2021-02-20 15:33:05 +0800

    Update Assets.java

--- BB2/src/gamev3/gfx/Assets.java
+++ BB2/src/gamev3/gfx/Assets.java
@@ -6,8 +6,9 @@ import java.awt.image.BufferedImage;
 public class Assets {
 
 	//some dimensions to represent width and height of each stickman in the spritesheet
-	private static final int width = 20, height = 40, w=55;
+	private static final int width = 20, height = 40;
 	private static final int buttonwidth = 598, buttonheight = 196;
+		
 	//all the image variables
 	public static BufferedImage player, player2, background, rocks, fireRocks,iceRocks,bootRocks,heartRocks,menu;
 	public static BufferedImage[] player_right, player_left, player_still,player2_left,player2_right,player2_still;
@@ -16,11 +17,10 @@ public class Assets {
 	//this init function is so that the game loads in everything once, instead of having
 	//this in the GameMain loops as it is very inefficient
 	public static void init() {
-		//SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/stickman-anim.png"));
-		SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/newChar.png"));
+		SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/stickman-anim.png"));
 		SpriteSheet sheet2 = new SpriteSheet(ImageLoader.loadImage("/textures/player2.png"));
 		SpriteSheet rock = new SpriteSheet(ImageLoader.loadImage("/textures/rocks.png"));
-		SpriteSheet start_button_icon = new SpriteSheet(ImageLoader.loadImage("/textures/start_button.png"));
+		SpriteSheet start_button_icon = new SpriteSheet(ImageLoader.loadImage("/textures/spritesheet.png"));
 		
 		//trying to add button
 		button_start = new BufferedImage[2];
@@ -34,17 +34,6 @@ public class Assets {
 		SpriteSheet heartRock = new SpriteSheet(ImageLoader.loadImage("/textures/heart.png"));
 		
 		//animation sprite
-		player_still = new BufferedImage[1];
-		player_still[0] = sheet.crop(0, 0, w, 49);
-		
-		player_right = new BufferedImage[1];
-		player_right[0] = sheet.crop(100,0 , w, 49);
-		
-		player_left = new BufferedImage[1];
-		player_left[0] = sheet.crop(0, 0, w, 49);
-		
-		//animation sprite
-		/*
 		player2_right = new BufferedImage[8];
 		player2_right[0] = sheet2.crop(0, 40, width, height);
 		player2_right[1] = sheet2.crop(20, 40, width, height);
@@ -105,8 +94,6 @@ public class Assets {
 		player_still[5] = sheet.crop(100, 120, width, height);
 		player_still[6] = sheet.crop(120, 120, width, height);
 		player_still[7] = sheet.crop(140, 120, width, height);
-		
-		*/
 		
 		//player = sheet.crop(width, 3*height, width, height);
 		//player2 = sheet.crop(width,  3*height, width, height);
