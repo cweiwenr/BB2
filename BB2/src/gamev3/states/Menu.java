0ff05f6152d0cd23f8346c9b5c4a8b2a855ea2e1
Nigouz <75168577+Nigouz@users.noreply.github.com>
2021-02-22 13:50:57 +0800

    Update Menu.java

--- BB2/src/gamev3/states/Menu.java
+++ BB2/src/gamev3/states/Menu.java
@@ -1,10 +1,12 @@
 package gamev3.states;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import gamev3.Handler;
 import gamev3.states.MenuTest;
+import gamev3.display.PixelFont;
 import gamev3.gfx.Assets;
 import gamev3.ui.ClickListener;
 import gamev3.ui.UIImageButton;
@@ -27,7 +29,7 @@ public class Menu extends State {
 
 			// Just creating an instance of this click listener class on the flag?
 			@Override
-			public void onClick() {
+			public void onClick() {	
 				State.setState(handler.getGame().gameState);
 			}
 		}));
@@ -44,6 +46,16 @@ public class Menu extends State {
 	public void render(Graphics g) {
 		menu.render(g);
 		uiManager.render(g);
+		g.setColor(new Color(255,255,255,128));
+		g.fillRect(320, 300, 410, 130);
+		g.setFont(new Font("pixel", Font.PLAIN, 14));
+		g.setColor(Color.black);
+		g.drawString("P1 (Left player) moves with 'A' and 'D' key",340,320);
+		g.drawString("P2 (Right player) moves with 'Left' and 'Right' arrow key",340,340);
+		g.drawString("Avoid the grey rocks which deducts your HP and points.",340,360);
+		g.drawString("Collect the other rocks for points and unknown boosts!",340,380);
+		g.drawString("Player with the highest points wins!",340,400);
+		g.drawString("Game ends when 1 player dies, so strategize well!",340,420);	
 	}
 
 }
